5f5d72ddfb21e071dec7dee87dd32889ed775275
Maxim Baturov <maxim.baturov@gmail.com>
2025-10-29 16:02:26 +0200

    s3-dev-works

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,8 @@ void initAccessPoint();
 //display
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define SDA_PIN 15
-#define SCL_PIN 6
+#define SDA_PIN 21
+#define SCL_PIN 19
 #define OLED_ADDR 0x3C
 #define OLED_RESET -1
 
